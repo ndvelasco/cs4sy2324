d68b1ddedde5a8cb163e61e9710f95733f603d87
CHARM <CHARM@172.22.16.100>
2023-08-29 16:13:53 +0800

    First java project

---
+++ src/cs4charmvelasco/CS4CharmVelasco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4charmvelasco;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4CharmVelasco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Whats up guys";
+        System.out.println(greeting + "Sup");
+    }
+    
+}
